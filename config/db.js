b8fcb1af15a0cdb72ec9e726a58449249a234bcc
salemes58 <sbasumatas58@gmail.com>
2023-01-19 16:32:25 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 const connect =async()=>{
   return mongoose.connect("mongodb+srv://salemes:salemes@cluster0.8hhyd86.mongodb.net/Mocks?retryWrites=true&w=majority");
 }
